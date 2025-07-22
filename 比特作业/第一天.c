97e472625c8957d2d8e343a9d71707f074c57c6e
FiyWine317 <3372058835@qq.com>
2025-07-22 14:11:01 +0800

    提交代码测试

--- 比特作业/第一天.c
+++ 比特作业/第一天.c
@@ -2,13 +2,18 @@
 //求两个数的较大值,写一个函数求两个整数的较大值
 // 输入：10 20,输出较大值：20
 #include<stdio.h>
-int bijiao(int a,int b)
-{
-	return (a > b) ? a:b;
-}
+//int bijiao(int a,int b)
+//{
+//	return (a > b) ? a:b;
+//}
+//int main()
+//{
+//	int a = 10, b = 20;
+//	printf("%d",bijiao(a, b));
+//	return 0;
+//}
 int main()
 {
-	int a = 10, b = 20;
-	printf("%d",bijiao(a, b));
+	printf("唐巧丽是猪！");
 	return 0;
 }
